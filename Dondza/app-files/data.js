38bfcfe689b3584d64a59f95ebd60e3109a095a3
CremildoJose <cremildojose14@gmail.com>
2020-12-01 00:02:39 +0200

    submit

---
+++ Dondza/app-files/data.js
@@ -0,0 +1,296 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrada-isutc",
+      "name": "Entrada Isutc",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1875,
+      "initialViewParameters": {
+        "yaw": 0.04926384847426668,
+        "pitch": -0.17762788675800856,
+        "fov": 1.1295095994859854
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.08163981214840987,
+          "pitch": -0.3311987598854227,
+          "rotation": 0,
+          "target": "1-ptio-exterior"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-ptio-exterior",
+      "name": "Pátio Exterior",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1875,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9731153892294646,
+          "pitch": -0.5905511512408665,
+          "rotation": 0,
+          "target": "2-ptio-d2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-ptio-d2",
+      "name": "Pátio D2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1875,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.513787899873729,
+          "pitch": -0.46334500567737,
+          "rotation": 0,
+          "target": "3-entrada-do-edifcio-d2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-entrada-do-edifcio-d2",
+      "name": "Entrada do Edifício D2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1875,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "4-corredor-de-baixo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-corredor-de-baixo",
+      "name": "Corredor de Baixo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.525409204358537,
+          "pitch": -0.15195286415213616,
+          "rotation": 0,
+          "target": "6-corredor-de-cima"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-sala-208",
+      "name": "Sala 208",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7133464303826997,
+          "pitch": -0.280977374229856,
+          "rotation": 0,
+          "target": "6-corredor-de-cima"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6088177925869989,
+          "pitch": 0.10666670560937241,
+          "title": "Projector",
+          "text": "É utilizado em todas aulas em que apresenta-se conteúdos digitais"
+        },
+        {
+          "yaw": -0.23091516568803172,
+          "pitch": -0.9541937457770828,
+          "title": "Ar Condicionado",
+          "text": "Todas salas dispõe de AC's com vista a criar o melhor ambiente de estudo"
+        }
+      ]
+    },
+    {
+      "id": "6-corredor-de-cima",
+      "name": "Corredor de Cima",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5280648544019648,
+          "pitch": -0.34291533299905197,
+          "rotation": 0,
+          "target": "5-sala-208"
+        },
+        {
+          "yaw": -2.0446612365766654,
+          "pitch": 0.0910524869933873,
+          "rotation": 0,
+          "target": "4-corredor-de-baixo"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Dondza Kola",
+  "settings": {
+    "mouseViewMode": "qtvr",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
